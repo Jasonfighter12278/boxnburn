787921386ecfead7bae5d9989a715e14d2f2ed03
Jasonfighter12278 <vansonhoventristan@gmail.com>
2025-09-07 17:53:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
+// Open Modal
 function openModal(plan) {
-    document.getElementById("subscription-modal").style.display = "flex";
-    document.getElementById("plan-input").value = plan;
+    document.getElementById('subscription-modal').style.display = "block";
+    document.getElementById('plan').value = plan;
 }
 
+// Close Modal
 function closeModal() {
-    document.getElementById("subscription-modal").style.display = "none";
+    document.getElementById('subscription-modal').style.display = "none";
 }
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    var formData = new FormData(this);
-    
-    fetch('send_email.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("form-status").innerText = data;
-    })
-    .catch(error => {
-        document.getElementById("form-status").innerText = "There was an error sending your message.";
-    });
-});
